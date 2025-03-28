e895967874c4ce82e71a51e1eef34387df1f9ed1
Frank Calabretta <netkinetic@gmail.com>
2025-03-28 05:23:29 -0400

    Delete

--- src/store/wizardState.ts
+++ src/store/wizardState.ts
@@ -1,24 +1,44 @@
+// src/store/wizardState.ts
 import { create } from 'zustand';
+import { Playbook, CampaignContent, CampaignData } from '@/types';
 
 interface WizardState {
   industry: string;
   businessType: string;
   goal: string;
-  campaignContent?: any; // <-- added
+  playbook: Playbook | null;
+  campaignData: CampaignData;
+  campaignContent: CampaignContent | null;
+
   setIndustry: (industry: string) => void;
   setBusinessType: (type: string) => void;
   setGoal: (goal: string) => void;
-  setCampaignContent: (content: any) => void; // <-- added
+  setSelectedPlaybook: (pb: Playbook) => void;
+  setCampaignData: (data: CampaignData) => void;
+  setGeneratedContent: (content: CampaignContent) => void;
 }
 
 export const useWizardState = create<WizardState>((set) => ({
   industry: '',
   businessType: '',
   goal: '',
-  campaignContent: null, // <-- default
+  playbook: null,
+  campaignData: {
+    campaignTitle: '',
+    businessName: '',
+    website: '',
+    keywords: '',
+    goal: '',
+    contentLength: 'short',
+    additionalInstructions: '',
+    playbookDuration: '30 days',
+  },
+  campaignContent: null,
+
   setIndustry: (industry) => set({ industry }),
   setBusinessType: (type) => set({ businessType: type }),
   setGoal: (goal) => set({ goal }),
-  setCampaignContent: (content) => set({ campaignContent: content }), // <-- added
+  setSelectedPlaybook: (pb) => set({ playbook: pb }),
+  setCampaignData: (data) => set({ campaignData: data }),
+  setGeneratedContent: (content) => set({ campaignContent: content }),
 }));
-
